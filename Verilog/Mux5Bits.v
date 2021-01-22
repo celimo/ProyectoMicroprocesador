753d537373b65f38b118e6546ef283b40a26c3c3
Celimo Porras Aguilar <68404113+celimo@users.noreply.github.com>
2021-01-22 13:56:57 -0600

    Actualización
    El anterior archivo de Mux5Bits no tenía comentarios, se agregan los comentarios del módulo

--- Verilog/Mux5Bits.v
+++ Verilog/Mux5Bits.v
@@ -18,6 +18,22 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+/*
+Objetivo del módulo
+Se crea para selecciona entre el valor de cero y el valor de los bits 19:15 de las instrucciones
+y ya que solo son 5 bits se debe crear otro módulo aparte del otro mux de 2 a 1
+
+-Entradas
+A: Primera entrada del mux 1'b0
+B: Segunda entrada del mux 1'b1
+sel: Señal de control para seleccionar entre las entradas
+
+-Salida
+O: Contiene el dato de alguna de las entradas, dependiendo de la señal de control
+
+*/
+
 module Mux5Bits(A, B, O, sel
     );
 	//Definicion de entradas y salidas 
